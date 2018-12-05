1c451b941a588d22c8e56e26a14c2c143a24c237
Patrick Gray <pgrayobx@gmail.com>
2018-12-05 11:32:04 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python2 cnn_predict_stack.py DukeTurtle_info.mat DukeTurtle_test.mat
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python2 cnn_predict_stack.py DukeTurtle_test.mat
